eae53b3a0191502cf1ede938f24dbbd4bbd54a60
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-12 21:07:37 +0100

    possibly sorted the gravity effect on height

--- ballistic_spec.js
+++ ballistic_spec.js
@@ -1,17 +1,27 @@
 var assert = require( 'assert' );
 var missile = require( './ballistics' );
 
-describe('Ballistics Tests', function(){
-  it('Say hello', function(){
-    assert.equal("Hello Master", missile.welcome() );
+describe( 'Link Test', function(){
+  it( 'Say hello', function() { 
+    assert.equal( "Hello Master", missile.welcome() );
   })
 })
 
-describe('Maths Tests', function(){
-  it('Horizontal Speed', function(){
+describe( 'Ballistics Tests', function() {
+  it( 'Horizontal Speed', function() {
     assert.equal( 25, missile.horizontal( 50, 60 ) );
   })
 
-  it()
+  it( 'Vertical Speed', function() {
+    assert.equal( 43.3, missile.vertical( 50, 60 ) );
+  })
+
+  it( 'Distance', function() {
+    assert.equal( 25, missile.distance( 25, 1 ) );
+  })
+
+  it( 'Height', function() {
+    assert.equal( 57.2, missile.height( 43.3, 8.8) );
+  }) 
 
 })
--- ballistics.js
+++ ballistics.js
@@ -6,15 +6,37 @@ var missile = {
   },
   gravity: 9.8,
   horizontal: function( speed, angle ) {
-    var sum = angle * degrees
-    var answer = speed * Math.cos( sum )
-    return Math.round( answer * 100) / 100
+    var sum = angle * degrees;
+    var answer = speed * Math.cos( sum );
+    return Math.round( answer * 100) / 100;
   },
+
   vertical: function( speed, angle ) {
-    var sum = angle * degrees
-    var answer = speed * Math.sin( sum )
-    return Math.round( answer * 100 ) / 100
+    var sum = angle * degrees;
+    var answer = speed * Math.sin( sum );
+    return Math.round( answer * 100 ) / 100;
+  },
+
+  distance: function( speed, time ) {
+    var answer = speed * time;
+    return answer;
+  },
+
+  height: function( speed, time ) {
+
+    var answer = ( speed * time ) - ( 0.5 * this.gravity * time * time )
+
+
+
+    // var sum = this.gravity * time;
+
+    // var answer = ( ( speed * time ) - ( time * this.gravity ) );
+    // console.log( speed * time )
+    return answer;
   }
+
+
+
 }
 
 
